62646637cac31edd95687cea7ad55e95e892338d
John Godley <johng75@gmail.com>
2011-01-19 07:38:53 +0000

    Change license text
    git-svn-id: http://plugins.svn.wordpress.org/audit-trail/trunk@334477 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- audit-trail.php
+++ audit-trail.php
@@ -10,14 +10,10 @@ Author URI: http://urbangiraffe.com
 This software is provided "as is" and any express or implied warranties, including, but not limited to, the
 implied warranties of merchantibility and fitness for a particular purpose are disclaimed. In no event shall
 the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or
-consequential damages (including, but not limited to, procurement of substitute goods or services; loss of
+consequential damages(including, but not limited to, procurement of substitute goods or services; loss of
 use, data, or profits; or business interruption) however caused and on any theory of liability, whether in
-contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of
+contract, strict liability, or tort(including negligence or otherwise) arising in any way out of the use of
 this software, even if advised of the possibility of such damage.
-
-This software is provided free-to-use, but is not free software.  The copyright and ownership remains entirely
-with the author.  Please distribute and use as necessary, in a personal or commercial environment, but it cannot
-be sold or re-used without express consent from the author.
 ============================================================================================================ 
 
 Available filters are:
